54cf194ed9203a38ee0bebea5384467c477c5fbc
Drew Folta <drew@folta.net>
2014-09-19 01:11:41 -0700

    layout

---
+++ yelp/BusinessCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessCell.swift
+//  yelp
+//
+//  Created by Andrew Folta on 9/19/14.
+//  Copyright (c) 2014 Andrew Folta. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- yelp/SearchResultsViewController.swift
+++ yelp/SearchResultsViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class SearchResultsViewController: UIViewController {
-    @IBOutlet weak var filterButton: UIButton!
+class SearchResultsViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var searchBar: UISearchBar!
-
-
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    @IBAction func onFilter(sender: AnyObject) {
+        println("--filter")
+    }
 
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        println("--searchBar --searchButton --text \(searchBar.text)")
+        searchBar.resignFirstResponder()
+    }
 
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        println("--searchBar --cancelButton --text \(searchBar.text)")
+    }
+
+    @IBAction func onFilter() {
+        println("--filter")
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = self.tableView.dequeueReusableCellWithIdentifier("BusinessCell", forIndexPath: indexPath) as BusinessCell
+        return cell
+    }
 
 //    override func didReceiveMemoryWarning() {
 //        super.didReceiveMemoryWarning()
