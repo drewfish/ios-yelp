2947185b519634464e92f9ef467acc6fb64feb63
Drew Folta <drew@folta.net>
2014-09-19 00:05:28 -0700

    starting layout, pink search

---
+++ yelp/SearchResultsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SearchResultsViewController.swift
+//  yelp
+//
+//  Created by Andrew Folta on 9/18/14.
+//  Copyright (c) 2014 Andrew Folta. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+    @IBOutlet weak var filterButton: UIButton!
+    @IBOutlet weak var searchBar: UISearchBar!
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//    }
+
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+//    }
+
+
+}
