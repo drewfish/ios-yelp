a89cd4bd5ea8fc288a89e780154e96e411ae6f32
Drew Folta <drew@folta.net>
2014-09-21 21:59:14 -0700

    autolayout of search results page

--- yelp/SearchResultsViewController.swift
+++ yelp/SearchResultsViewController.swift
@@ -31,6 +31,7 @@ class SearchResultsViewController: UIViewController, UISearchBarDelegate, UITabl
         searchBar.delegate = self
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
         search()
     }
 
